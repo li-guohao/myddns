7d94af83c90088c4f98b6c47854fcaf89b02de8a
li-guohao <46225881+li-guohao@users.noreply.github.com>
2022-07-18 09:30:02 +0800

    Update MyDdnsScript.java

--- src/main/java/cn/liguohao/myddns/MyDdnsScript.java
+++ src/main/java/cn/liguohao/myddns/MyDdnsScript.java
@@ -60,9 +60,9 @@ public class MyDdnsScript {
         DOMAIN_PREFIX_MAP.put("container", "Portainer");
         DOMAIN_PREFIX_MAP.put("git", "Gitea");
         DOMAIN_PREFIX_MAP.put("nas", "QnapNas");
-        DOMAIN_PREFIX_MAP.put("@", "Home Page");
-        DOMAIN_PREFIX_MAP.put("www", "Home Page");
-        DOMAIN_PREFIX_MAP.put("ci", "Drone");
+        DOMAIN_PREFIX_MAP.put("@", "HomePage");
+        DOMAIN_PREFIX_MAP.put("www", "HomePage");
+        DOMAIN_PREFIX_MAP.put("ci", "Jenkins");
 
     }
 
